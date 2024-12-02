f3c90207df55909464d5732c197b1efda9323a05
Lorentzon <noxxet.13@hotmail.se>
2024-12-02 15:00:07 +0100

    sup fresher

--- scripts/button.gd
+++ scripts/button.gd
@@ -1,33 +1,30 @@
 extends Node3D
-@export var target : Node3D
-@export var pressed_by_default : bool
+
+@export var targets: Array[Node3D] = [] # Enforce that targets must be Node3D
+@export var pressed_by_default: bool
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-var button_state : bool = false
+var button_state: bool = false
 @onready var cooldown_timer: Timer = $CooldownTimer
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if pressed_by_default:
 		button_state = true
-		animation_player.play("toggle-on",-1,10.0)
-	
-		if target and target.has_method("receive_input"):
-			target.receive_input(button_state)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+		animation_player.play("toggle-on", -1, 10.0)
+		
+		# Send input to all targets
+		for target in targets:
+			if target and target.has_method("receive_input"):
+				target.receive_input(button_state)
 
 func _on_button_area_3d_area_entered(_area: Area3D) -> void:
 	if cooldown_timer.is_stopped():
 		cooldown_timer.start()
 		if not animation_player.is_playing():
-			if button_state:
-				button_state = false
-				animation_player.play("toggle-off")
-			else:
-				button_state = true
-				animation_player.play("toggle-on")
+			# Toggle the button state
+			button_state = not button_state
+			animation_player.play("toggle-on" if button_state else "toggle-off")
 			
-			if target and target.has_method("receive_input"):
-				target.receive_input(button_state)
+			# Send input to all targets
+			for target in targets:
+				if target and target.has_method("receive_input"):
+					target.receive_input(button_state)
